1f3669a446cda49ce5d6cd3203d4457983a9079b
Lilly81324 <simkue2@web.de>
2025-01-30 18:25:08 +0100

    Aktualisieren von ft_display_map.c

--- src/ft_display_map.c
+++ src/ft_display_map.c
@@ -12,23 +12,6 @@
 
 #include "so_long.h"
 
-// turns each character from the map object
-// into the respective image from the image object
-void	*ft_cont_to_img(t_win *win, char in)
-{
-	if (in == '1')
-		return (win->img->wall);
-	else if (in == '0')
-		return (win->img->empty);
-	else if (in == 'C')
-		return (win->img->coin);
-	else if (in == 'P')
-		return (win->img->player);
-	else if (in == 'E')
-		return (win->img->exit);
-	return (win->img->player);
-}
-
 // draws the map by putting images in each position
 // limits are map height and map width
 void	ft_display_map(t_win *win)
