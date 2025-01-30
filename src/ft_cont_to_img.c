4e86eeba69cf856fcb409be52a9c9615e4be3a3f
Lilly81324 <simkue2@web.de>
2025-01-30 18:25:30 +0100

    Aktualisieren von ft_cont_to_img.c

--- src/ft_cont_to_img.c
+++ src/ft_cont_to_img.c
@@ -1,1 +1,18 @@
 #include "so_long.h
+
+// turns each character from the map object
+// into the respective image from the image object
+void	*ft_cont_to_img(t_win *win, char in)
+{
+	if (in == '1')
+		return (win->img->wall);
+	else if (in == '0')
+		return (win->img->empty);
+	else if (in == 'C')
+		return (win->img->coin);
+	else if (in == 'P')
+		return (win->img->player);
+	else if (in == 'E')
+		return (win->img->exit);
+	return (win->img->player);
+}
