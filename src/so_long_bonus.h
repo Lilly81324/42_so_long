8b5ff9495d781fa3c771b35b31421f3122eaba60
Lilly81324 <simkue2@web.de>
2025-01-31 18:57:19 +0100

    Aktualisieren von so_long_bonus.h

--- src/so_long_bonus.h
+++ src/so_long_bonus.h
@@ -6,5 +6,6 @@
 int        ft_check_map_bonus(t_map *map);
 int        ft_check_map_count_bonus(t_map *map);
 int        ft_run_game_bonus(t_map *map);
+int        ft_move_bonus(t_win *win, int dir);
 
 #endif
