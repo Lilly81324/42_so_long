3153e6a4ef71f17e68d19b48b066b2129565fd53
Lilly K. Home <likunne@DESKTOP-QBDTI2D.>
2025-02-06 23:48:25 +0100

    Slight tweaks to libft, removing unncecesay code, further details, see libft

--- libft/ft_toupper.c
+++ libft/ft_toupper.c
@@ -6,15 +6,12 @@
 /*   By: sikunne <sikunne@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 10:35:41 by sikunne           #+#    #+#             */
-/*   Updated: 2024/11/11 13:16:06 by sikunne          ###   ########.fr       */
+/*   Updated: 2025/02/06 23:43:25 by sikunne          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_toupper(int x)
 {
-	int	i;
-
-	i = 0;
 	if (x <= 122 && x >= 97)
 		return (x - 32);
 	return (x);
